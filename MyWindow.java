c03a361e59b1a2af3fd58a098fe87f363d29536b
Michael Bench <mbench6342@pointloma.edu>
2014-10-20 15:56:20 -0700

    Added file output choosing window

--- MyWindow.java
+++ MyWindow.java
@@ -4,6 +4,11 @@ import javax.swing.*;
 
 public class MyWindow {
    private JFrame mainFrame;
+	private JPanel outputPanel;
+	private JButton chooseOutput;
+	private JLabel chooseOutputLabel;
+	private JFileChooser fcOutput;
+	
  
    public MyWindow(){
       mainFrame = new JFrame("Java Swing Examples");
@@ -13,10 +18,36 @@ public class MyWindow {
       mainFrame.setVisible(true);
 		
 		setupFileInput();
+		setupFileOutput();
+		
    }
 	
 	private void setupFileInput() {
 	
+	}
+	
+	private void setupFileOutput() {
+		outputPanel = new JPanel();
+		outputPanel.setLayout(new FlowLayout());
+		
+		chooseOutputLabel = new JLabel("Specifiy An Output Directory");
+		outputPanel.add(chooseOutputLabel);
+		
+		chooseOutput = new JButton("Choose");
+		chooseOutput.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fcOutput = new JFileChooser();
+				JFrame fileChooser = new JFrame();
+				fileChooser.add(fcOutput);
+				fileChooser.setSize(500,500);
+				fileChooser.setVisible(true);
+			}
+		});
+		outputPanel.add(chooseOutput);
+		
+		outputPanel.setVisible(true);
+		mainFrame.add(outputPanel);
+		
 	}
 	
 	 public static void main(String[] args){
