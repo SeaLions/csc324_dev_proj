f5fd4207a280f67aab3345aa6152792e6ded4307
Aaron McKinstry <aaronmckinstry706@pointloma.edu>
2014-11-03 16:16:55 -0800

    fixing merge conflicts.

--- MyWindow.java
+++ MyWindow.java
@@ -1,83 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-<<<<<<< HEAD
-import java.text.*;
-import java.io.PrintWriter;
 import java.io.File;
-import java.io.IOException;
-=======
-import java.io.File;
->>>>>>> GUI
 
 public class MyWindow implements ActionListener {
    
    private JFrame mainFrame;
-<<<<<<< HEAD
-   private JFrame alertFrame;
-	private JPanel outputPanel;
-   private JPanel controlPanel;
-	private JButton chooseOutputButton;
-	private JLabel chooseOutputButtonLabel;
-	private JFileChooser outputDirChooser;
-   private UserInput userInput;
-   private JPanel inputPanel;
-   private JButton chooseOutput;
-   private JLabel chooseOutputLabel, input1, input2, statusLabel;
-   private JFileChooser fcOutput;
-   private File RFPSFile, SignalProFile;//instantiated when kml files are selected on GUI
- 
-   
-   //class variable to hold the output filepath
-   //initially set to current directory
-   String datapath = "./"; 
- 
-   public MyWindow(){
-      statusLabel = new JLabel("",JLabel.CENTER);
-
-      controlPanel = new JPanel();
-		controlPanel.setLayout(new FlowLayout());
-      
-      inputPanel = new JPanel();
-      inputPanel.setLayout(new FlowLayout());
-
-      mainFrame = new JFrame("Compare Signal Coverage");
-      mainFrame.setSize(800,800);
-      Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-      mainFrame.setLocation(dim.width/2-mainFrame.getSize().width/2, dim.height/2-mainFrame.getSize().height/2);
-      mainFrame.setLayout(new FlowLayout());
-      mainFrame.addWindowListener(new WindowAdapter(){
-         public void windowClosing(WindowEvent windowEvent){
-            System.exit(0);
-         }
-      });
-        
-      mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
-      mainFrame.add(statusLabel);
-      mainFrame.add(controlPanel);
-      mainFrame.add(inputPanel);
-      mainFrame.setVisible(true);
-            
-      setupFileInput();
-      setupFileOutput();
-   }//end MyWindow()
-   
-   
-   //Method to insert KML files
-   public String getKML() throws IOException {   
-      FileDialog fd = new FileDialog(mainFrame, "Choose a file", FileDialog.LOAD);
-      fd.setDirectory("C:\\");
-      fd.setFile("*.kml");
-      fd.setVisible(true);
-      String filename = fd.getFile();
-      if (filename == null)
-        filename = "You cancelled the operation";
-      return filename;
-     
-   }// End getKML method
-   
-   //Start showInputButton() function
-=======
 	private JFrame alertFrame;
    private JLabel statusLabel;
    private JPanel controlPanel;
@@ -109,10 +37,7 @@ public class MyWindow implements ActionListener {
       mainFrame.setVisible(true);
    }
 	
-	//Start showInputButton() function
->>>>>>> GUI
-   //Method that has 2 buttons to input one kmz and one kml file
-   private void setupInputOptions(){
+	private void setupInputOptions(){
       
       GridBagConstraints constraints = new GridBagConstraints();
       constraints.anchor = GridBagConstraints.LINE_START;
@@ -140,15 +65,8 @@ public class MyWindow implements ActionListener {
       mainFrame.add(signalProInputLabel, constraints);
       
    }//end showInputButton() function
-<<<<<<< HEAD
-	
-  
-   private void showRunButton(){
-      JButton runButton = new JButton("RUN");
-=======
 
 	private void setupOutputOptions() {
->>>>>>> GUI
       
       GridBagConstraints constraints = new GridBagConstraints();
       constraints.anchor = GridBagConstraints.LINE_START;
@@ -164,13 +82,7 @@ public class MyWindow implements ActionListener {
       constraints.gridy = 2;
 		mainFrame.add(directoryOutputLabel, constraints);
       
-<<<<<<< HEAD
-      controlPanel.add(runButton);
-      mainFrame.setVisible(true);  
-   }//end showRunButton() function
-=======
 	}
->>>>>>> GUI
    
    private void setupRunControls() {
       
@@ -183,19 +95,6 @@ public class MyWindow implements ActionListener {
       runButton.addActionListener(this); 
 		mainFrame.add(runButton, constraints);
       
-<<<<<<< HEAD
-      try{
-         PrintWriter writer = new PrintWriter(fileHandler);
-         writer.println(kmlString);
-         writer.close();
-			alertWindow("Saved KML file as '" + datapath + "outfile.kml'");
-         //statusLabel.setText("Saved KML file as '" + datapath + "outfile.kml'");
-      }catch (Exception e){
-			alertWindow("There was a problem creating the file.");
-         //statusLabel.setText("There was a problem creating the file.");
-      }
-=======
->>>>>>> GUI
    }
    
    private void setupResultsDisplay() {
@@ -210,83 +109,13 @@ public class MyWindow implements ActionListener {
       mainFrame.add(outputLocationLabel, constraints);
       
    }
-<<<<<<< HEAD
-	
-	 // ********************MAIN METHOD**********************
-    public static void main(String[] args)
-    {
-  	     MyWindow program = new MyWindow();
-		 
-        program.showRunButton();
-        program.showInputButton();
-	 }
-=======
    
    public void actionPerformed(ActionEvent e) {
->>>>>>> GUI
    
-    public void actionPerformed(ActionEvent e)
-    {
-        //Handle open button action.
-        if (e.getSource() == chooseOutputButton) {
-            int returnVal = outputDirChooser.showOpenDialog(mainFrame);
+      //Handle open button action.
+      if (e.getSource() == chooseOutputButton) {
+         int returnVal = outputDirChooser.showOpenDialog(mainFrame);
          
-<<<<<<< HEAD
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File directory = outputDirChooser.getSelectedFile();
-                userInput.setOutputDirectory(directory);
-            }
-        }
-    }
-
-	 private void setupFileOutput()
-    {
-        outputPanel = new JPanel();
-		  outputPanel.setLayout(new FlowLayout());
-		
-		  chooseOutputButtonLabel = new JLabel("Specifiy An Output Directory");
-		  outputPanel.add(chooseOutputButtonLabel);
-		
-		  chooseOutputButton = new JButton("Choose");
-		  chooseOutputButton.addActionListener(this);
-		  outputPanel.add(chooseOutputButton);
-		
-		  mainFrame.add(outputPanel);
-		  outputPanel.setVisible(true);
-    }
-  
-   
-    public void alertWindow(String message)
-    {
-        JLabel alertLabel = new JLabel("Alert",JLabel.CENTER);
-		  alertLabel.setText(message);
-		
-		  JPanel okPanel = new JPanel();
-		  okPanel.setLayout(new FlowLayout());
-		  JButton okButton = new JButton("OK");
-        okButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) { 
-			       alertFrame.dispose();
-            }          
-        });
-		  okPanel.add(okButton);
-		  alertFrame = new JFrame("Alert");
-        alertFrame.setSize(500,150);
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        alertFrame.setLocation(dim.width/2-alertFrame.getSize().width/2, dim.height/2-alertFrame.getSize().height);
-        GridLayout gridLayout = new GridLayout(0,1);
-		  alertFrame.setLayout(gridLayout);
-        alertFrame.addWindowListener(new WindowAdapter(){
-            public void windowClosing(WindowEvent windowEvent){
-                alertFrame.dispose();
-            }
-        });
-        alertFrame.setDefaultCloseOperation(alertFrame.DISPOSE_ON_CLOSE);
-		  alertFrame.add(alertLabel);
-		  alertFrame.add(okPanel);
-        alertFrame.setVisible(true);
-   }//end alertWindow()
-=======
          if (returnVal == JFileChooser.APPROVE_OPTION) {
             File directory = outputDirChooser.getSelectedFile();
             userInput.setOutputDirectory(directory);
@@ -350,6 +179,5 @@ public class MyWindow implements ActionListener {
 	 public static void main(String[] args){
   		MyWindow demo = new MyWindow( );
    }
->>>>>>> GUI
 
 }
