089701faf3199272faf4abe41b49e97fb62de3dc
Aaron McKinstry <aaronmckinstry706@pointloma.edu>
2014-11-07 12:53:13 -0800

    Continued work on SignalPro Processing

--- SignalProData.java
+++ SignalProData.java
@@ -1,12 +1,9 @@
 import java.io.*;
-import org.jsoup.*;
 import java.util.*;
 import java.util.zip.*;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 public class SignalProData extends PlotData
@@ -20,9 +17,33 @@ public class SignalProData extends PlotData
    {
       try {
          Document kmlTree = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input);
+         NodeList nl = kmlTree.getElementsByTagName("north");
+                  
+         if (nl != null && nl.getLength() > 0) {
+      	   Node n = nl.item(0).getChildNodes().item(0);
+            //NodeList coordinates = el.getChildNodes();
+                        
+            /*Node northNode = coordinates.item(0);
+            double northCoordinate = Double.parseDouble(northNode.getTextContent()); 
+            
+            Node southNode = coordinates.item(1);
+            double southCoordinate = Double.parseDouble(southNode.getTextContent());
+            
+            Node eastNode = coordinates.item(2);
+            double eastCoordinate = Double.parseDouble(eastNode.getTextContent());
+            
+            Node westNode = coordinates.item(3);
+            double westCoordinate = Double.parseDouble(westNode.getTextContent());*/
+            
+            //System.out.println(northCoordinate);
+            //System.out.println(southCoordinate);
+            //System.out.println(eastCoordinate);
+            //System.out.println(westCoordinate);
+   		}
          
       }
       catch (Exception e) {
+         System.out.println(e);
       }
    }
    
@@ -33,7 +54,6 @@ public class SignalProData extends PlotData
          Enumeration<? extends ZipEntry> entries = kmz.entries();
          while (entries.hasMoreElements()) {
             ZipEntry entry = entries.nextElement();
-            System.out.println(entry.getName());
             if (true) {
                
                processKml(kmz.getInputStream(entry));
